9d676fd65a4e9fd70140b547999d67f231d2f315
elaqaoui <mohamed.elaqaoui@gmaim.com>
2024-04-28 20:17:29 +0100

    creating file 1 2 3

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -9,7 +9,7 @@ from flasgger.utils import swag_from
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/amenity/all_amenities.yml')
-def get_amenities():
+def get_all_amenities():
     """
     Retrieves a list of all amenities
     """
@@ -22,8 +22,8 @@ def get_amenities():
 
 @app_views.route('/amenities/<amenity_id>/', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/get_amenity.yml', methods=['GET'])
-def get_amenity(amenity_id):
+@swag_from('documentation/amenity/get_all_amenity.yml', methods=['GET'])
+def get_all_amenity(amenity_id):
     """ Retrieves an amenity """
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
@@ -52,8 +52,8 @@ def delete_amenity(amenity_id):
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/amenity/post_amenity.yml', methods=['POST'])
-def post_amenity():
+@swag_from('documentation/amenity/posting_amenity.yml', methods=['POST'])
+def posting_amenity():
     """
     Creates an amenity
     """
@@ -71,8 +71,8 @@ def post_amenity():
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/put_amenity.yml', methods=['PUT'])
-def put_amenity(amenity_id):
+@swag_from('documentation/amenity/puting_amenity.yml', methods=['PUT'])
+def puting_amenity(amenity_id):
     """
     Updates an amenity
     """
